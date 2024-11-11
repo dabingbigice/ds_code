decdbe2693aff5d96e08670537cbef80c0e42a9e
lab273 <838523724@qq.com>
2024-11-11 20:42:57 +0800

    bug

--- src/_2024_11/binary_search.java
+++ src/_2024_11/binary_search.java
@@ -4,28 +4,28 @@ public class binary_search {
 
     public static int func(int[] a, int target) {
 
-        int index_i=0;
-        int index_j=a.length-1;
-        int index_m = (index_j+index_i)/2;
-        while (index_i<=index_j){
+        int index_i = 0;
+        int index_j = a.length - 1;
+        int index_m = (index_j + index_i) >> 1;
+        while (index_i <= index_j) {
             //=因为当 i= m = j 的时候有可能正好是目标值
-            if (a[index_m]==target){
-                return 1;
+            if (a[index_m] == target) {
+                return index_m;
             }
-            if (index_m>target){
-                index_j=index_m-1;
+            if (target < index_m) {
+                index_j = index_m - 1;
             }
-            if (index_m<target){
-                index_i=index_m+1;
+            if (index_m < target) {
+                index_i = index_m + 1;
             }
-            index_m = (index_j+index_i)/2;
+            index_m = (index_j + index_i) >> 1;
         }
 
         return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println(func(new int[]{1,3,6,9,10,12},10));//1
+        System.out.println(func(new int[]{1, 3, 6, 9, 10, 12}, 10));//1
     }
 }
 
