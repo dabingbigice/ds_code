0fd7f2b25bb1e87f182bb39e9f3b2c202fda5243
838523724@qq.com <838523724@qq.com>
2024-12-25 17:31:17 +0800

    二叉树

--- src/com/ds/_2024_12/二叉树_非递归遍历/_101_对称二叉树.java
+++ src/com/ds/_2024_12/二叉树_非递归遍历/_101_对称二叉树.java
@@ -32,4 +32,18 @@ public class _101_对称二叉树 {
         return (left.val == right.val) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
 
     }
+
+    public boolean isSymmetric1(TreeNode root) {
+        if (root == null) return true;
+
+        return isMirror1(root.left, root.right);
+    }
+
+    private boolean isMirror1(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if ((left == null && right != null) || (right == null && left != null)) return false;
+        if (left.val != right.val) return false;
+        //如果相等就还需要继续对比左右孩子
+        return isMirror1(left.left, right.right) && isMirror1(left.right, right.left);
+    }
 }
